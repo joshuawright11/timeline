ea9f31f89aeb17539a7cbb37d5310de949b22e22
Josh Wright <joshuawright11@gmail.com>
2014-01-29 22:06:09 -0600

    Added database helper to connect to database. Changed classpath, added libs folder.

---
+++ src/backend/DBHelper.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package backend;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * @author Josh Wright
+ * Created: Jan 29, 2014
+ * Package: backend
+ *
+ */
+public class DBHelper {
+	private Connection connection = null;
+	private ResultSet resultSet = null;  
+    private Statement statement = null; 
+	private String dbName;
+	DBHelper(String dbName){
+		this.dbName = dbName;
+		try {
+			connectToDB(this.dbName);
+		} catch (SQLException e) {
+			System.out.println("Couldn't connect to database.");
+			e.printStackTrace();
+		} catch (ClassNotFoundException e){
+			System.out.println("Class not found. Check your classpath for JDBC.");
+			e.printStackTrace();
+		}
+	}
+	private void connectToDB(String dbName) throws SQLException, ClassNotFoundException{
+		Class.forName("org.sqlite.JDBC");
+		connection = DriverManager.getConnection("jdbc:sqlite:"+this.dbName+"");
+		statement = connection.createStatement();
+	}
+	public ResultSet doSomthing(String query) throws SQLException{
+		resultSet = statement.executeQuery(query);
+		return resultSet;
+	}
+}
--- src/backend/Driver.java
+++ src/backend/Driver.java
@@ -3,6 +3,8 @@
  */
 package backend;
 
+import java.sql.SQLException;
+
 /**
  * @author Josh Wright
  * Created: Jan 28, 2014
@@ -15,8 +17,12 @@ public class Driver {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		DBHelper helper = new DBHelper("timeline.db");
+		try {
+			helper.doSomthing("create table t2 (t1key INTEGER PRIMARY KEY,data TEXT,num double,timeEnter DATE);");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
