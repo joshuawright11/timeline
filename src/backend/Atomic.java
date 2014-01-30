3899b91adbcb4c8accb158b8847e151268ff8418
Josh Wright <joshuawright11@gmail.com>
2014-01-29 20:24:59 -0600

    Added abstact class for events, as well as concrete implementations Duration and Atomic.

---
+++ src/backend/Atomic.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package backend;
+import java.sql.Date;
+/**
+ * @author Josh Wright
+ * Created: Jan 29, 2014
+ * Package: backend
+ *
+ */
+public class Atomic extends TLEvent {
+	private Date date;
+	Atomic(String name, Date date){
+		super(name);
+		this.setDate(date);
+	}
+	/**
+	 * @return the date
+	 */
+	public Date getDate() {
+		return date;
+	}
+	/**
+	 * @param date the date to set
+	 */
+	public void setDate(Date date) {
+		this.date = date;
+	}
+}
