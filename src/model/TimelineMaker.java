a19a86776f290f2c76d9dc0f04acf93709d643c5
joshuawright11 <joshuawright11@gmail.com>
2014-02-01 14:48:55 -0600

    Added TimelineMaker class to model the entire timeline maker. This will have instantiations of the TimelineGraphics, OptionsDispatch, and TLOptionsDispatch classes.

---
+++ src/model/TimelineMaker.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package model;
+
+/**
+ * @author josh.l.wright
+ *
+ */
+public class TimelineMaker implements Runnable {
+	private TLOptionsDispatch timelineOptions;
+	private TimelineGraphics timelineGraphics;
+	private OptionsDispatch options;
+	void createEvent(){
+		//timelineOptions.addEvent(event);
+	}
+	@Override
+	public void run() {
+		
+	}
+}
