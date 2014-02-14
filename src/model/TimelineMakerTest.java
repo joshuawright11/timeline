9e2883805e844ae4d40c2723f3735eb530b73a6f
disanto14 <andrew.d.thompson@my.wheaton.edu>
2014-02-13 20:37:00 -0600

    Added TimelineMakerTest.java

---
+++ src/model/TimelineMakerTest.java
@@ -0,0 +1,59 @@
+package model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TimelineMakerTest {
+
+	@Test
+	public void testTimelineMaker() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTimelineTitles() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSelectedTimeline() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetSelectedTimeline() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddTimeline() {
+		assertTrue("The timeline added should be in timelines array.", false);
+	}
+
+	@Test
+	public void testRemoveSelectedTimeline() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSelectedEvent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetSelectedEvent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveSelectedEvent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateGraphics() {
+		fail("Not yet implemented");
+	}
+
+}
