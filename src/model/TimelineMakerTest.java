b6a31d6d6163314647983aac4c7f2d4df178f393
disanto14 <andrew.d.thompson@my.wheaton.edu>
2014-02-15 11:38:39 -0600

    Completed JUnit test for TimelineMaker.

--- src/model/TimelineMakerTest.java
+++ src/model/TimelineMakerTest.java
@@ -2,6 +2,8 @@ package model;
 
 import static org.junit.Assert.*;
 
+import java.sql.Date;
+
 import org.junit.Test;
 
 import database.DBHelper;
@@ -11,6 +13,9 @@ import entities.*;
  * TimelineMakerTest.java
  * 
  * This class tests the functionality of the TimelineMaker class.
+ * The tests are a little contrived because the TimelineMaker is interacting
+ * with the database, GUI, and graphics objects, so these tests had to be
+ * simplified slightly.
  * 
  * @author Andrew Thompson
  * Wheaton College, CS 335, Spring 2014
@@ -19,28 +24,144 @@ import entities.*;
  */
 public class TimelineMakerTest {
 	
-	static TimelineMaker app;
-	
-	static {
-		System.out.println("Hi");
-		DBHelper db = new DBHelper("databases/timelinemakertest.db");
-		while (db.getTimelines().length != 0)
-			db.removeTimeline(db.getTimelines()[0]);
-		app = new TimelineMaker(db);
-	}
-	
+	/**
+	 * Ensure that the TimelineMaker constructor instantiates correctly.
+	 */
 	@Test
 	public void testTimelineMakerInit() {
+		TimelineMaker app = null;
+		assertNull(app);
+		app = new TimelineMaker(new DBHelper("databases/timelinemakertest.db"));
 		assertNotNull(app);
 	}
 	
+	/**
+	 * Ensure that a new Timeline is correctly added to a TimelineMaker.
+	 */
 	@Test
-	public void testTimelineAdditionDeletion() {
+	public void testTimelineAddition() {
+		TimelineMaker app = new TimelineMaker(new DBHelper("databases/timelinemakertest.db"));
 		Timeline test = new Timeline("Test");
-		assertFalse(app.getTimelineTitles().contains("Test"));
+		assertFalse(app.getTimelineTitles().contains(test.getName()));
+		assertNull(app.getSelectedTimeline());
 		app.addTimeline(test);
-		assertTrue(app.getTimelineTitles().contains("Test"));
+		assertTrue(app.getTimelineTitles().contains(test.getName()));
+		assertTrue(app.getSelectedTimeline().equals(test));
+	}
+	
+	/**
+	 * Ensure the selected timeline of the TimelineMaker is correctly deleted.
+	 */
+	@Test
+	public void testTimelineDeletion() {
+		TimelineMaker app = new TimelineMaker(new DBHelper("databases/timelinemakertest.db"));
+		Timeline test = new Timeline("Test");
+		app.addTimeline(test);
+		assertTrue(app.getTimelineTitles().contains(test.getName()));
+		assertTrue(app.getSelectedTimeline().equals(test));
 		app.deleteTimeline();
-		assertFalse(app.getTimelineTitles().contains("Test"));
+		assertFalse(app.getTimelineTitles().contains(test.getName()));
+		assertNull(app.getSelectedTimeline());
+	}
+	
+	/**
+	 * Ensure TimelineMaker handles timeline selection correctly.
+	 */
+	@Test
+	public void testTimelineSelection() {
+		TimelineMaker app = new TimelineMaker(new DBHelper("databases/timelinemakertest.db"));
+		Timeline test1 = new Timeline("Test1");
+		app.addTimeline(test1);
+		app.selectTimeline(test1.getName());
+		assertTrue(app.getSelectedTimeline().equals(test1));
+	}
+	
+	/**
+	 * Ensure the selected timeline of the TimelineMaker is correctly edited.
+	 */
+	@Test
+	public void testTimelineEditing() {
+		TimelineMaker app = new TimelineMaker(new DBHelper("databases/timelinemakertest.db"));
+		Timeline test1 = new Timeline("Test1");
+		Timeline test2 = new Timeline("Test2", test1.getEvents(), 0);
+		app.addTimeline(test1);
+		assertTrue(app.getTimelineTitles().contains(test1.getName()));
+		app.selectTimeline(test1.getName());
+		assertTrue(app.getSelectedTimeline().equals(test1));
+		app.editTimeline(test2);
+		assertFalse(app.getTimelineTitles().contains(test1.getName()));
+		assertTrue(app.getTimelineTitles().contains(test2.getName()));
+		app.selectTimeline(test2.getName());
+		assertTrue(app.getSelectedTimeline().equals(test2));
+	}
+	
+	/**
+	 * Ensure a new event is added to the selected timeline correctly.
+	 */
+	@Test
+	public void testEventAddition() {
+		TimelineMaker app = new TimelineMaker(new DBHelper("databases/timelinemakertest.db"));
+		Timeline test = new Timeline("Test");
+		Atomic event1 = new Atomic("event1", "category1", new Date(0));
+		Duration event2 = new Duration("event2", "category2", new Date(0), new Date(100000));
+		app.addTimeline(test);
+		assertFalse(app.getSelectedTimeline().contains(event1));
+		assertFalse(app.getSelectedTimeline().contains(event2));
+		assertNull(app.getSelectedEvent());
+		app.addEvent(event1);
+		assertTrue(app.getSelectedTimeline().contains(event1));
+		assertTrue(app.getSelectedEvent().equals(event1));
+		app.addEvent(event2);
+		assertTrue(app.getSelectedTimeline().contains(event2));
+		assertTrue(app.getSelectedEvent().equals(event2));
+	}
+	
+	/**
+	 * Ensure the selected event is correctly deleted.
+	 */
+	@Test
+	public void testEventDeletion() {
+		TimelineMaker app = new TimelineMaker(new DBHelper("databases/timelinemakertest.db"));
+		Timeline test = new Timeline("Test");
+		Atomic event = new Atomic("event1", "category1", new Date(0));
+		app.addTimeline(test);
+		app.addEvent(event);
+		assertTrue(app.getSelectedTimeline().contains(event));
+		assertTrue(app.getSelectedEvent().equals(event));
+		app.deleteEvent();
+		assertFalse(app.getSelectedTimeline().contains(event));
+		assertNull(app.getSelectedEvent());
+	}
+	
+	/**
+	 * Ensure events are selected correctly.
+	 */
+	@Test
+	public void testEventSelection() {
+		TimelineMaker app = new TimelineMaker(new DBHelper("databases/timelinemakertest.db"));
+		Timeline test = new Timeline("Test");
+		Atomic event = new Atomic("event1", "category1", new Date(0));
+		app.addTimeline(test);
+		app.addEvent(event);
+		app.selectEvent(event);
+		assertTrue(app.getSelectedEvent().equals(event));
+	}
+	
+	/**
+	 * Ensure events are edited correctly.
+	 */
+	@Test
+	public void testEventEditing() {
+		TimelineMaker app = new TimelineMaker(new DBHelper("databases/timelinemakertest.db"));
+		Timeline test = new Timeline("Test");
+		Atomic event1 = new Atomic("event1", "category1", new Date(0));
+		app.addTimeline(test);
+		app.addEvent(event1);
+		app.selectEvent(event1);
+		Duration event2 = new Duration("event2", "category2", new Date(0), new Date(100));
+		app.editEvent(event2);
+		assertTrue(app.getSelectedTimeline().contains(event2));
+		assertTrue(app.getSelectedEvent().equals(event2));
+		
 	}
 }
